ec6ad33a9e26243926f95ee4eb36e5a105cfe573
CAMILLEG <cgyrd@outlook.fr>
2025-01-07 17:11:25 +0100

    adding caroussel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import { Link } from "react-router-dom";
 
-
-
 function App() {
   return (
     <>
       <div>
-        <Link to="./avatar"> Avatar</Link>
+        <Link to="./avatar"> Conçois ton avatar !</Link>
       </div>
     </>
   );
