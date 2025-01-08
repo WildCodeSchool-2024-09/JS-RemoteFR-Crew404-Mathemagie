acdaf74921cbcbc223a023575b21f198d3cc9bd3
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2025-01-08 10:29:17 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,8 @@ import ForgotPassword from "./components/ForgotPassword";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 
-function App () {
-  return (
-    <SignUpForm />
-  );
+function App() {
+  return <SignUpForm />;
 }
 
 export default App;
