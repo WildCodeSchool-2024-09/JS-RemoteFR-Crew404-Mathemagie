b80530d29bf9dea3edd9875c67913f200ae9fa39
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2025-01-08 09:32:15 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ForgotPassword from "./components/ForgotPassword";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 
-const App = () => {
+function App () {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/sign-up" element={<SignUpForm />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+    <SignUpForm />
   );
 };
 
