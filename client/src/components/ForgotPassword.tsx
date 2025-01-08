2e0d46126791f2bb87299b84ef16ea4e1ae8ed38
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2025-01-08 11:32:20 +0100

    Update ForgotPassword.tsx

--- client/src/components/ForgotPassword.tsx
+++ client/src/components/ForgotPassword.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ForgotPassword.css";
 
-function ForgotPassword () {
+function ForgotPassword() {
   const [email, setEmail] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
