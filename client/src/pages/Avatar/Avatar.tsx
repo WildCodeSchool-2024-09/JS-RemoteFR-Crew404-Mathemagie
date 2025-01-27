f368687012e313440460c661ab141d3720a4184d
camilleg <cgyrd@outlook.fr>
2025-01-27 15:48:45 +0100

    Update Avatar.tsx

--- client/src/pages/Avatar/Avatar.tsx
+++ client/src/pages/Avatar/Avatar.tsx
@@ -26,7 +26,9 @@ function Avatar() {
     e.preventDefault();
 
     try {
- const response = await fetch(`${import.meta.env.VITE_API_URL}/api/avatar`, {
+ const response = await fetch(
+   `${import.meta.env.VITE_API_URL}/api/avatar`, 
+   {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
