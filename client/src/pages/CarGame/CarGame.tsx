11c0ca4327fe2c11ce26a7da37a222502d1d68dc
Marc Juvénal <marcjuvenal95@gmail.com>
2025-02-06 00:40:56 +0100

    boutons je sais pas

--- client/src/pages/CarGame/CarGame.tsx
+++ client/src/pages/CarGame/CarGame.tsx
@@ -17,62 +17,62 @@ const questions = [
     question: "Il y'a combien de voitures ?",
     answer: 10,
     image: caisse,
-    options: [4, 5, 2, 10],
+    options: [4, 5, 10, "Je ne sais pas"],
   },
   {
     question: "Il y'a combien de voitures noires ?",
     answer: 2,
     image: caisse,
-    options: [4, 5, 2, 6],
+    options: [4, 5, 2, "Je ne sais pas"],
   },
 
   {
     question: "Compte les billets de 20€ ?",
     answer: 7,
     image: s,
-    options: [7, 6, 5, 4],
+    options: [7, 6, 5, "Je ne sais pas"],
   },
   {
     question: "Compte le nombre d'animaux ?",
     answer: 9,
     image: animals,
-    options: [11, 8, 9, 6],
+    options: [11, 8, 9, "Je ne sais pas"],
   },
   {
     question: "Quelle est la somme des 2 billets ?",
     answer: 100,
     image: somme,
-    options: [90, 100, 73, 64],
+    options: [90, 100, 73, "Je ne sais pas"],
   },
   {
     question: "Quel est le nombre de voitures décapotables ?",
     answer: 4,
     image: decapo,
-    options: [2, 4, 6, 7],
+    options: [2, 4, 6, "Je ne sais pas"],
   },
   {
     question: "Combien d'enfants ont les cheveux de couleur noire' ?",
     answer: 3,
     image: enfants,
-    options: [8, 6, 3, 4],
+    options: [8, 6, 3, "Je ne sais pas"],
   },
   {
     question: "Quelle est la somme de ces 2 billets ?",
     answer: 35,
     image: billets,
-    options: [32, 34, 43, 35],
+    options: [32, 35, 43, "Je ne sais pas"],
   },
   {
     question: "Combien de roues possède chaque moto ?",
     answer: 2,
     image: moto,
-    options: [2, 3, 7, 5],
+    options: [2, 3, 7, "Je ne sais pas"],
   },
   {
     question: "Combien de vitres possède ce bus ?",
     answer: 10,
     image: busBus,
-    options: [8, 12, 9, 10],
+    options: [8, 12, 10, "Je ne sais pas"],
   },
 ];
 
@@ -90,8 +90,8 @@ function CarGame() {
     setCurrentQuestion(questions[questionIndex]);
   }, [questionIndex]);
 
-  const handleChoice = (answer: number) => {
-    setSelectedAnswer(answer);
+  const handleChoice = (answer: number | string) => {
+    setSelectedAnswer(typeof answer === 'number' ? answer : null);
     if (answer === currentQuestion.answer) {
       setScore(score + 1);
       setFeedback("correct");
@@ -208,7 +208,10 @@ function CarGame() {
 
       <h1>Associe les objets au bon chiffre !</h1>
 
-      <div className="question" style={{ color: "#4059ad" }}>
+      <div
+        className="question"
+        style={{ color: "#4059ad", fontSize: "1.5rem" }}
+      >
         {currentQuestion.question}
         <img
           src={currentQuestion.image}
